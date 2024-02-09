777a6320ac384a6da131face3df34bec1fbd65c3
YelveTejas <tejas.yelve20@gmail.com>
2024-02-09 15:37:05 +0530

    Updated resume

--- src/Components/resume.js
+++ src/Components/resume.js
@@ -1,6 +1,7 @@
 import Resume from '../images/Updated_resume.pdf'
 import { saveAs } from 'file-saver';
 const downloadFile = () => {
+  
     window.open("https://drive.google.com/file/d/1OLxTThKBDvk85P1UsMgej-E8EQRAfm-E/view?usp=sharing", "_blank")
     const filename = 'Tejas_yelve_resume.pdf'; // the name of the file you want to download
     const file = Resume; // the path to the file you want to download
